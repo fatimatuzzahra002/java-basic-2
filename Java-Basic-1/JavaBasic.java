20367056d944ebbb9f8ad0632d200404c339647f
fatimatuzzahra002 <fatimatuzzahra035@gmail.com>
2021-10-12 16:43:45 +0800

    commit first challenge

---
+++ Java-Basic-1/JavaBasic.java
@@ -0,0 +1,16 @@
+public class JavaBasic {
+    public static void main(String[]args){
+        String FirstName = "Fatimatuz";
+        String LastName = "Zohrah";
+        int Age = 21;
+        String PhoneNumber = "087750501508";
+        String Address = "Lenggorong Desa Sambik Elen, Lombok Utara";
+
+        System.out.println("First Name : "+ FirstName);
+        System.out.println("Last Name : "+ LastName);
+        System.out.println("Age :" + Age);
+        System.out.println("Phone Number :" + PhoneNumber);
+        System.out.println("Adress :" + Address);
+    }
+    
+}
