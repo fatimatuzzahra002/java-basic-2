95a23b31ba86be37dadea925c02c0953a799ad87
fatimatuzzahra002 <fatimatuzzahra035@gmail.com>
2021-10-16 11:34:54 +0800

    Java Challenge 3

---
+++ JavaBasic3/JavaBasic3.java
@@ -0,0 +1,18 @@
+public class JavaBasic3 {
+    public static void main(String[] args) {
+        int z = 5;
+            for (int y = 1; y <= z; y++) {
+                for (int u = 4; u >= y; u--) {
+                    System.out.print(' ');
+                }
+                for (int t = 1; t <= y; t++) {
+                    System.out.print("$");
+                }
+                for ( int k = 1; k <= y - k; k++){
+                    System.out.print("$");
+                }
+                System.out.println();
+            }
+    }
+
+}
